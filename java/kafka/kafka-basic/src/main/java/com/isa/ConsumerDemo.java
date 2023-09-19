579f04aa0e4a469a33d443fb5dcb7c4ffe407ee8
mohsssen <i.mohsssen@gmail.com>
2023-09-19 11:43:20 +0200

    add consumer classic

--- java/kafka/kafka-basic/src/main/java/com/isa/ConsumerDemo.java
+++ java/kafka/kafka-basic/src/main/java/com/isa/ConsumerDemo.java
@@ -1,23 +1,27 @@
 package com.isa;
 
-import org.apache.kafka.clients.producer.Callback;
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.time.Duration;
+import java.util.Arrays;
 import java.util.Properties;
 
-public class ProducerWithKeys {
-    private final static Logger log = LoggerFactory.getLogger(ProducerWithKeys.class.getSimpleName());
+public class ConsumerDemo {
+    private final static Logger log = LoggerFactory.getLogger(ConsumerDemo.class.getSimpleName());
 
     public static void main(String[] args) {
-        log.debug("Call Back demo");
+        log.debug("Kafka Consumer");
+        String groupId = "my-java-app";
+        String topic = "demo_java";
         // create producer properties
         Properties prop = new Properties();
-        prop.setProperty("bootstrap.servers", "127.0.0.1:9092");
+        //prop.setProperty("bootstrap.servers", "127.0.0.1:9092");
 
         /*
         https://issa.conduktor.app/admin/my-playground
@@ -32,44 +36,26 @@ public class ProducerWithKeys {
         prop.setProperty("security.protocol", "SASL_SSL");
         prop.setProperty("sasl.jaas.config", "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"w22YdpPqqt9urXwJVjgvZ\" password=\"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2F1dGguY29uZHVrdG9yLmlvIiwic291cmNlQXBwbGljYXRpb24iOiJhZG1pbiIsInVzZXJNYWlsIjpudWxsLCJwYXlsb2FkIjp7InZhbGlkRm9yVXNlcm5hbWUiOiJ3MjJZZHBQcXF0OXVyWHdKVmpndloiLCJvcmdhbml6YXRpb25JZCI6NzY2ODksInVzZXJJZCI6ODkyMjYsImZvckV4cGlyYXRpb25DaGVjayI6IjZhNGU5ZWE5LWNjNmYtNDE5NC1iMGJiLTNlNDg1OGQ0N2VkYiJ9fQ.4ZU_0RDtYCKPfEfeQORkdmdStRdXYzceWJGLIN9yTbA\";");
         prop.setProperty("sasl.mechanism", "PLAIN");
-        prop.setProperty("key.serializer", StringSerializer.class.getName());
-        prop.setProperty("value.serializer", StringSerializer.class.getName());
 
+        // Create a consumer
+        prop.setProperty("key.deserializer", StringDeserializer.class.getName());
+        prop.setProperty("value.deserializer", StringDeserializer.class.getName());
+        prop.setProperty("group.id", groupId);
+        prop.setProperty("auto.offset.reset", "earliest");
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(prop);
+        consumer.subscribe(Arrays.asList(topic));
+        while (true) {
+            log.info("================================ Polling ============================");
+            ConsumerRecords<String, String> records =  consumer.poll(Duration.ofMillis(1000));
 
-        // create a producer
-        try {
-            KafkaProducer<String, String> producer = new KafkaProducer<>(prop);
-            for (int i = 0; i < 10; i++) {
-                String topic = "demo_java";
-                String key = "id_"+i;
-                String val = "demo  value"+i;
-                ProducerRecord<String, String> producerRecord = new ProducerRecord<>(topic, key, val);
-
-                // send data
-                producer.send(producerRecord, new Callback() {
-                    @Override
-                    public void onCompletion(RecordMetadata metadata, Exception exception) {
-                        // executed every time a record successfully sent or an exception is thrown
-                        if (exception == null) {
-                            log.info("meta data key {}\npartition: {}, offset: {}, timestamp:{}  ",
-                                    key, metadata.partition(), metadata.offset(), metadata.timestamp());
-                        } else {
-                            log.error("error sending {}", exception);
-                        }
-                    }
-                });
-            }
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+            for (ConsumerRecord<String, String> rec : records ) {
+                log.info("key: {}, value: {}\nPartition:{}, offset: {}",
+                        rec.key(), rec.value(), rec.partition(), rec.offset());
             }
 
-            producer.flush();
-            producer.close();
-        } catch (Exception e) {
-            log.error("Error Sending", e);
+
         }
+
 
 
     }
